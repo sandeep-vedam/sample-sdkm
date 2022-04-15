917191633a6fb3ccba5b5758917696ae24362cf7
Sandeep Sharma Vedam <55154896+sandeep-vedam@users.noreply.github.com>
2022-04-15 10:14:47 +0530

    Create index.js

---
+++ src/SdkPlugins/index.js
@@ -0,0 +1,31 @@
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2020 Metrological
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export let log
+export let settings
+export let appInstance
+export let lightning
+
+export const initSdkPlugin = (AppInstance, logs, setting, lightnings ) => {
+  log = logs
+  settings = setting
+  lightning = lightnings
+  appInstance = AppInstance
+}
+
